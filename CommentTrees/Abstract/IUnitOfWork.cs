d4c32080d0e673cd5b751bfbd9e6155a21dce7b2
TheMifort <serejat1996@rambler.ru>
2019-12-04 14:18:53 +0300

    UnitOfWork

---
+++ CommentTrees/Abstract/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using CommentTrees.Models.Database;
+
+namespace CommentTrees.Abstract
+{
+    public interface IUnitOfWork
+    {
+        IRepository<Item> ItemRepository { get; }
+        IRepository<Comment> CommentRepository { get; }
+    }
+}
---
+++ CommentTrees/UnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using System.Data;
+using CommentTrees.Abstract;
+using CommentTrees.Models.Database;
+using CommentTrees.Repositories;
+
+namespace CommentTrees
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public IRepository<Item> ItemRepository { get; }
+        public IRepository<Comment> CommentRepository { get; }
+
+        public UnitOfWork(IDbConnection dbConnection)
+        {
+            ItemRepository = new ItemRepository(dbConnection);
+            CommentRepository = new CommentRepository(dbConnection);
+        }
+    }
+}
