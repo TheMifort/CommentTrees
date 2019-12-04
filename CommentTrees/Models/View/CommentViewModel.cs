f306d74260f17e9537a1ebf1cf96510671324145
TheMifort <serejat1996@rambler.ru>
2019-12-04 13:02:42 +0300

    View models

---
+++ CommentTrees/Models/View/CommentViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace CommentTrees.Models.View
+{
+    public class CommentViewModel
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public IEnumerable<CommentViewModel> Comments { get; set; }
+    }
+}
---
+++ CommentTrees/Models/View/ItemViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace CommentTrees.Models.View
+{
+    public class ItemViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public IEnumerable<CommentViewModel> Comments { get; set; }
+    }
+}
