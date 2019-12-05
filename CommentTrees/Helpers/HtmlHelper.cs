886fdaabd2b38cf432fa0e79c96a75f0db58b8c6
TheMifort <serejat1996@rambler.ru>
2019-12-05 16:31:21 +0300

    Design changes

--- CommentTrees/Helpers/HtmlHelper.cs
+++ CommentTrees/Helpers/HtmlHelper.cs
@@ -22,7 +22,7 @@ namespace CommentTrees.Helpers
         private static void PrintTreeNode(this StringBuilder sb, CommentViewModel comment, int itemId)
         {
             sb.Append("<div style=\"margin-left: 10px; margin-top: 4px; margin-bottom:2px; padding:2px; border: 1px solid black;\">");
-            sb.Append(comment.Id).Append(":").Append(comment.Text);
+            sb.Append(comment.Id).Append(": ").Append(comment.Text);
             sb.Append("<a style=\"float: right;\" href=Delete/").Append(itemId).Append("/").Append(comment.Id).Append(">Delete</a>");
             sb.Append("<div style=\"margin: 3px;\">")
                 .Append("<form method=\"post\" action=\"/Comment\">")
